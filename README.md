0c3d510f13ea6ad7e8255be6b116bae02bd2692c
liaopingping <liaopingping@sansaas.com>
2018-01-31 18:52:08 +0800

    可修改任何样式的select

--- README.md
+++ README.md
@@ -1,31 +1,75 @@
 ### 介绍 
-1. 这个插件主要是提供样式美化的,所以代码都会标注得很详细,以供大家一目了然,之后针对自己特殊的业务需求做拓展 
-2. 支持滚动条样式修改,滚动条啊,太麻烦了,就算能改变原生滚动条的样式,也不能改变滚动条的占位,所以内容看起来总是缺一块,所以这里的滚动条也是画的,支持各种样式
-#### 原理
-因为浏览器的兼容问题,select这个标签本身是无法通过改变样式去达到现在所需求的美化界面,所以还是需要重新画一个
-
-自定义滚动条就是让宽度+18px,让原生滚动条看不见,之后自己画一个滚动条
-#### 目标 
-1. 尽量提供多的API可以改变select的样式以供任何项目使用 
-2. 兼容所有现代浏览器 
-3. 尽量提供多方法 
+这个插件主要是提供样式美化的,可以修改任何样式,包括箭头和滚动条
 
 #### 使用方法 
-1. 先确保引入jquery,并且在当前脚本之前引入
-```html
-2. 在结构上,还是原来的select标签使用方法:
+使用示例看sample.html
 
-``` 
+### 详细说明(可不看,最基本的用法看sample.html就可以)
+
+  除了optionAry是必填的,其他都有默认值,选填
+
 
 ```javascript
-之后js部分:
+ $(".target").beautifySelect({
+        optionAry:[ //option数组
+            {
+                val:"id1",//为<options></option> 的value
+                showTxt1:"A项目",//显示在li的内容
+                showTxt2:"9人投票" //如果有showText2 就在li的右边显示,可不填
+            },
+            {
+                val:"id2",
+                showTxt1:"B项目",
+                showTxt2:"239人投票"
+            },
+            {
+                val:"id3",
+                showTxt1:"C项目",
+                showTxt2:"349人投票"
+            }
 
-``` 
+        ],
+        trigger:"hover", //触发条件为hover,默认为click触发
+        defaultVal:"请选择", //select默认显示内容,
+        outer:{//最外围的div样式设置,就是在sample中的<div class="target"></div>
+             display: "inline-block",
+             position: "relative",
+              boxSizing: "border-box"
+        },
+        select:{
+            style:{//select的样式,驼峰写法,覆盖自定义样式
+                border: "1px solid #E0E2E4"
+            },
+            hoverStyle:{ //select的hover样式
+                
+            },
+            focusStyle:{//待完善
+                
+            }
+        },
+        optionsBorder:{//显示的下拉框样式
+          
+        },
+        option:{
+            //li的样式
+            style:{},
+            hoverStyle:{}
+        },
+        scrollBarStyle:{},//滚动条的样式,
+        triangle:{},//三角形的样式,可以通过修改样式,通过背景图片,把三角形改为箭头图片,
+        liSpan:{ //li可接受两个内容,一左一右显示
+            span1:{
+                style:{},
+                hoverStyle:{}
+            },
+            span2:{
+                style:{},
+                hoverStyle:{}
+            }
+        }
+
+    });
+```
+
 ### 后续
 不定期优化增强
-
-
-### 待续功能
-1. 双输入
-2. 三角形图片输入
-3. 设定默认select
--- src/jquery_beautifySelect.js
+++ src/jquery_beautifySelect.js
@@ -5,14 +5,15 @@
 
             //默认样式
             var opt = {
-                outer:{
+                outer: {
                     display: "inline-block",
-                    position:"relative",
-                    boxSizing:"border-box"
+                    position: "relative",
+                    boxSizing: "border-box"
                 },
                 select: {
                     style: {
                         display: "inline-block",
+                        position: "relative",
                         width: "200px",
                         height: "40px",
                         lineHeight: "40px",
@@ -21,7 +22,7 @@
                         borderRadius: "3px",
                         cursor: "pointer",
                         textAlign: "center",
-                        zIndex:999
+                        zIndex: 999
                     },
                     hoverStyle: {
                         border: "1px solid #E0E2E4"
@@ -36,22 +37,22 @@
                     left: "0",
                     top: "0",
                     boxSizing: "border-box",
-                    paddingTop:"50px",
-                    zIndex:-99,
-                    display:"none"
+                    paddingTop: "50px",
+                    zIndex: -99,
+                    display: "none"
                 },
-                optionsBorder:{
-                    maxHeight: "200px",
+                optionsBorder: {
+                    maxHeight: "204px",
                     width: "200",
                     border: "1px solid #E0E2E4",
                     boxSizing: "border-box",
-                    overflow:"hidden"
+                    overflow: "hidden"
                 },
-                options:{ //ul 的样式
+                options: { //ul 的样式
                     maxHeight: "220px",
                     width: "218px",
                     overflowY: "scroll",
-                    overflowX: "scroll",
+                    overflowX: "hidden",
 
                 },
                 option: { //li
@@ -59,49 +60,84 @@
                         height: "36px",
                         lineHeight: "36px",
                         fontSize: "16px",
-                        color:"#333",
+                        color: "#333",
                         backgroundColor: "#fff",
                         listStyle: "none",
-                        textAlign:"left",
-                        cursor:"pointer",
-                        paddingLeft:"15px"
+                        textAlign: "left",
+                        cursor: "pointer",
+                        paddingLeft: "15px"
                     },
                     hoverStyle: {
                         backgroundColor: "#F1F2F4"
                     },
                     focusStyle: {}
                 },
-                scrollBarStyle :{
-                    position:"absolute",
-                    top:"50px",
-                    right:"0",
-                    height:"200px",
-                    width:"6px",
-                    backgroundColor:"#eff1f3",
-                    borderRadius:"100px",
+                scrollBarStyle: {
+                    position: "absolute",
+                    top: "50px",
+                    right: "0",
+                    height: "200px",
+                    width: "6px",
+                    backgroundColor: "#eff1f3",
+                    borderRadius: "100px",
                 },
                 triangle: {
-                    style: {},
+                    style: {
+                        display: "block",
+                        position: "absolute",
+                        right: "12px",
+                        top: "50%",
+                        width: "0",
+                        height: "0",
+                        border: "6px solid transparent",
+                        borderTopColor: "#999999",
+                        zIndex:99999999999
+                    },
+                    animateEnterStyle: {
+                        transform: "rotate(180deg)",
+                        transition: "all 0.3s",
+                        transformOrigin: "50% 20%"
+                    },
+                    animateLeaveStyle: {
+                        transform: "rotate(0deg)",
+                        transition: "all 0.3s",
+                        transformOrigin: "50% 20%"
+                    },
                     hoverStyle: {},
                     focusStyle: {}
                 },
-                liSpan:{
-                    span1:{
-                        display:"inline-block",
-                        height:"100%",
-                        width:"40%"
+                liSpan: {
+                    span1: {
+                        style: {
+                            display: "inline-block",
+                            height: "100%",
+                            width: "40%",
+                            color: "#333",
+                            fontSize: "16px"
+                        },
+                        hoverStyle: {
+                            color: "#868686"
+                        }
                     },
-                    span2:{
-                        display:"inline-block",
-                        height:"100%",
-                        width:"50%",
-                        textAlign:"right"
+                    span2: {
+                        style: {
+                            display: "inline-block",
+                            height: "100%",
+                            width: "50%",
+                            textAlign: "right",
+                            color: "#333",
+                            fontSize: "12px"
+                        },
+                        hoverStyle: {
+                            color: "#868686"
+                        }
                     }
-                }
+                },
+                defaultVal: "请选择"
             };
 
             //合并数据
-            opt = $.extend(true,{},opt,options); //和外部输入样式合并
+            opt = $.extend(true, {}, opt, options); //和外部输入样式合并
 
             //设置结构
             let $this = $(this),//获取目标
@@ -110,6 +146,7 @@
                 $optionsBorder = $("<div></div>"),
                 $ul = $("<ul></ul>"),
                 $scrollBar = $("<div></div>"),
+                $triangle = $("<i></i>"),
                 $lists = opt.optionAry,
                 isHover = opt.trigger,
                 listsHeight = 0;
@@ -117,44 +154,92 @@
 
             //设置样式
             $this.css(opt.outer);
-            $select.css(opt.select.style); //select
+            $select.css(opt.select.style).html(opt.defaultVal); //select
             $optionsWrapper.css(opt.optionsWrapper);
             $optionsBorder.css(opt.optionsBorder);
             $ul.css(opt.options);
+            $triangle.css(opt.triangle.style);
             $scrollBar.css(opt.scrollBarStyle);
 
-            $this.append($select,$optionsWrapper);
+            $this.append($select, $optionsWrapper,$triangle);
             $optionsWrapper.append($optionsBorder);
-            $optionsBorder.append($ul,$scrollBar);
+            $optionsBorder.append($ul, $scrollBar);
 
-            //向ul塞入内容
-
-            var liFunction = function (target) {
+            var setSpanStyle = function ($li, type) { //为了li里面能放两个内容,所以要分别对li子元素的span进行设置
+                let children = $li.children(),
+                    isHover = type === 'hover';
+                for (let i = 0; i < children.length; i++) {
+                    if (i === 0) {
+                        isHover ?
+                            $(children[i]).css(opt.liSpan.span1.hoverStyle) :
+                            $(children[i]).css(opt.liSpan.span1.style);
+                    } else {
+                        isHover ?
+                            $(children[i]).css(opt.liSpan.span2.hoverStyle) :
+                            $(children[i]).css(opt.liSpan.span2.style);
+                    }
+                }
+            };
+            var liFunction = function (target) { //对下拉列表的li功能做绑定
                 target.hover(function () {
                     $(this).css(opt.option.hoverStyle);
+                    setSpanStyle($(this), "hover");
                 }, function () {
                     $(this).css(opt.option.style);
+                    setSpanStyle($(this));
                 }).click(() => {
                     let str = "";
                     str += target.children().html();
-                    $select.attr("value",target.attr("value")).html(str);
+                    $select.attr("value", target.attr("value")).html(str);
+                    $optionsWrapper.slideUp(300);
+                    triangleLeave();
                 });
             };
 
+            var showDropdown = function () { //列表下拉函数
+                var ulHeight = $ul.height(),
+                    ulTop = parseInt($optionsWrapper.css("paddingTop")),
+                    scrollHeight = Math.floor(parseInt(opt.scrollBarStyle.height) * 0.7),
+                    scrollBlank = parseInt(opt.scrollBarStyle.height) - scrollHeight,
+                    needScollbar = listsHeight-$ul.height();
+                if(needScollbar<=0){
+                    $scrollBar.hide();
+                    return;
+                }
+                $scrollBar.height(scrollHeight);
+                $ul.scroll(() => { //自定义滚动条滚动
+                    let scroTop = $ul.scrollTop(),
+                        outlength = listsHeight - ulHeight,
+                        outRate = scrollBlank * (scroTop / outlength);
+                    $scrollBar.css({
+                        top: ulTop + outRate
+                    });
+                });
+
+            };
+
+            var triangleEnter = function () {
+                $triangle.css(opt.triangle.animateEnterStyle);
+            };
+            var triangleLeave = function () {
+                $triangle.css(opt.triangle.animateLeaveStyle);
+            };
+
+            //向ul塞入内容
             $lists.forEach(obj => {
                 var _length = 0;
-                for(var key in obj){
-                    if(obj.hasOwnProperty(key)){
-                        _length ++;
-                        if(_length ===1){
-                            var $li = $("<li></li>").css(opt.option.style).attr("value",obj[key]);
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        _length++;
+                        if (_length === 1) {
+                            var $li = $("<li></li>").css(opt.option.style).attr("value", obj[key]);
                             listsHeight += parseInt($li.css("height"));
                             liFunction($li);
-                        }else if(_length ===2){
-                            var $span = $("<span></span>").html(obj[key]).css(opt.liSpan.span1);
+                        } else if (_length === 2) {
+                            var $span = $("<span></span>").html(obj[key]).css(opt.liSpan.span1.style);
                             $li.append($span);
-                        }else{
-                            $span = $("<span></span>").html(obj[key]).css(opt.liSpan.span2);
+                        } else {
+                            $span = $("<span></span>").html(obj[key]).css(opt.liSpan.span2.style);
                             $li.append($span);
                         }
                     }
@@ -162,39 +247,27 @@
                 $ul.append($li);
             });
 
-            var showDropdown = function () {
-                var ulHeight = $ul.height(),
-                    ulTop = parseInt($optionsWrapper.css("paddingTop")),
-                    scrollHeight = Math.floor(parseInt(opt.scrollBarStyle.height)*0.7),
-                    scrollBlank = parseInt(opt.scrollBarStyle.height)-scrollHeight;
-                $scrollBar.height(scrollHeight);
-                $ul.scroll( ()=> {
-                    let scroTop = $ul.scrollTop(),
-                        outlength = listsHeight- ulHeight,
-                        outRate = scrollBlank*(scroTop/outlength);
-                    $scrollBar.css({
-                        top:ulTop+outRate
-                    });
-                });
-            };
+
             showDropdown();
             //触发select的条件
-            if(isHover){
-                $select.hover(()=>{
+            if (isHover) {
+                $select.hover(() => {
                     $optionsWrapper.show();
                     $optionsBorder.slideDown(400);
                     showDropdown();
+                    triangleEnter();
                 });
-            }else{
-                $select.click(()=>{
+            } else {
+                $select.click(() => {
                     $optionsWrapper.show();
                     $optionsBorder.slideDown(400);
                     showDropdown();
+                    triangleEnter();
                 });
             }
-            $this.mouseleave(()=>{
+            $this.mouseleave(() => {
                 $optionsWrapper.slideUp(300);
-
+                triangleLeave();
             });
             return this;//返回jQuery本身
         };
